6addcb05bf45eb39a0722aca5a1e7af32a5f3522
Lucas <lgs.08lucas@gmail.com>
2025-07-05 09:06:06 -0300

    Criado função para converter Series para SeriesDTO, e criado mapeamento da url /series/top5 para enviar as top 5 séries mais bem avaliadas.

--- src/main/java/br/com/alura/screenmatch/controllers/SeriesController.java
+++ src/main/java/br/com/alura/screenmatch/controllers/SeriesController.java
@@ -4,17 +4,23 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.services.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@RequestMapping("/series")
 @RestController // Define classe como controller
 public class SeriesController {
     @Autowired
     private SerieService service; // Para acessar os services
 
-    @GetMapping("/series") // Cria um get para /series
+    @GetMapping // Cria um get para /series
     public List<SerieDTO> getSeries() { // O /series chamará esta função
         return service.getAllSeries(); // Solicita dados ao service
+    }
+
+    @GetMapping("/top5") // Fica /series/top5
+    public List<SerieDTO> getTop5Series() {
+        return service.getTop5Series();
     }
 }
--- src/main/java/br/com/alura/screenmatch/services/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/services/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.services;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.models.Series;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,14 @@ public class SerieService {
     private SerieRepository repository; // Para acesso ao banco de dados
 
     public List<SerieDTO> getAllSeries(){
-        return repository.findAll().stream().map(SerieDTO::new).toList();
+        return convertSeriesToDTO(repository.findAll());
+    }
+
+    public List<SerieDTO> getTop5Series() {
+        return convertSeriesToDTO(repository.findTop5ByOrderByRatingDesc());
+    }
+
+    private List<SerieDTO> convertSeriesToDTO(List<Series> series){
+        return series.stream().map(SerieDTO::new).toList();
     }
 }
